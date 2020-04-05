4e777a0bf2bf59033dd4ec406a95cc8556024378
winstonjones <winstonj@kth.se>
2020-04-05 16:27:59 +0200

    Implementing RCDB to communicate withing socket packets

--- RainServer/src/st/whineHouse/rainserver/Server.java
+++ RainServer/src/st/whineHouse/rainserver/Server.java
@@ -1,5 +1,7 @@
 package st.whineHouse.rainserver;
 
+import st.whineHouse.raincloud.serialization.RCDatabase;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -16,6 +18,9 @@ public class Server {
         this.port = port;
     }
 
+    private final int MAX_PACKET_SIZE = 1024;
+    private byte[] receivedDataBuffer = new byte[MAX_PACKET_SIZE * 10];
+
      public void start() {
         try {
             socket = new DatagramSocket(port);
@@ -25,20 +30,46 @@ public class Server {
         }
         listening = true;
 
-        listeningThread = new Thread(this::listen);
+        listeningThread = new Thread(this::listen, "RainServer-ListenThread");
         listeningThread.start();
-
      }
 
      private void listen() {
         while (listening) {
-
+            DatagramPacket packet = new DatagramPacket(receivedDataBuffer, MAX_PACKET_SIZE);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            process(packet);
         }
      }
 
      private void process(DatagramPacket packet) {
-
+        byte[] data = packet.getData();
+        if(new String(data, 0, 4).equals("RCDB")) {
+            RCDatabase database = RCDatabase.Deserialize(data);
+            String username = database.findObject("root").findString("username").getString();
+            process(database);
+        } else {
+            switch (data[0]) {
+                case 1:
+                    // connection packet
+                    break;
+                case 2:
+                    // ping packet
+                    break;
+                case 3:
+                    // login attempt packet
+                    break;
+            }
+        }
      }
+
+    private void process(RCDatabase database) {
+
+    }
 
      public void send(byte[] data, InetAddress address, int port) {
         assert(socket.isConnected());
